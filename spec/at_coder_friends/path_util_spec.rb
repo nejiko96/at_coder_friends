62b4a7d85dd1bd5c19e17a1a56042c28776f3d4f
nejiko96 <nejiko2006@gmail.com>
2018-02-18 08:10:01 +0900

    add PathUtil::contest_name test

--- spec/at_coder_friends/path_util_spec.rb
+++ spec/at_coder_friends/path_util_spec.rb
@@ -3,6 +3,29 @@
 RSpec.describe AtCoderFriends::PathUtil do
   subject(:path_util) { described_class }
 
+  spec_root = File.expand_path('..', __dir__)
+  contest_root = File.join(spec_root, 'fixtures/project/ARC#001')
+
+  describe '#contest_name' do
+    subject { path_util.contest_name(path) }
+
+    context 'from file' do
+      let(:path) { File.join(contest_root, 'A.rb') }
+
+      it 'returns contest name' do
+        expect(subject).to eq('arc001')
+      end
+    end
+
+    context 'from directory' do
+      let(:path) { contest_root }
+
+      it 'returns contest name' do
+        expect(subject).to eq('arc001')
+      end
+    end
+  end
+
   describe '#split_prg_path' do
     subject { path_util.split_prg_path(path) }
     let(:path) { '/foo/bar/contest/A_v2.rb' }
