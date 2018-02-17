94f65d12c4b6e8a6d776ccd2c41af165d2f5c663
nejiko96 <nejiko2006@gmail.com>
2018-02-17 09:19:00 +0900

    format_parser.rb refinement

--- lib/at_coder_friends/format_parser.rb
+++ lib/at_coder_friends/format_parser.rb
@@ -3,7 +3,77 @@
 require 'English'
 
 module AtCoderFriends
+  class Iterator
+    def initialize(array)
+      @array = array
+      @i = 0
+    end
+
+    def next?
+      @i < @array.size
+    end
+
+    def next
+      ret = @array[@i]
+      @i += 1
+      ret
+    end
+  end
+
   class FormatParser
+    PARSERS = [
+      {
+        type: :harray,
+        fmt: :number,
+        pat: /^(?<v>[a-z]+)[01](\s+\k<v>.)*(\s+\.+)?(\s+\k<v>.)+$/i,
+        vars: ->(m) { m[:v] },
+        pat2: ->(_) { nil },
+        size: ->(f) { f[-1] }
+      },
+      {
+        type: :harray,
+        fmt: :char,
+        pat: /^(?<v>[a-z]+)[01](\k<v>.)*(\s*\.+\s*)?(\k<v>.)+$/i,
+        vars: ->(m) { m[:v] },
+        pat2: ->(_) { nil },
+        size: ->(f) { f[-1] }
+      },
+      {
+        type: :matrix,
+        fmt: :number,
+        pat: /^(?<v>[a-z]+)[01][01](\s+\k<v>..)*(\s+\.+)?(\s+\k<v>..)+$/i,
+        vars: ->(m) { m[:v] },
+        pat2: ->(v) { /(^#{v}..(\s+#{v}..)*(\s+\.+)?(\s+#{v}..)+|\.+)$/ },
+        size: ->(f) { f[-2..-1].chars.to_a }
+      },
+      {
+        type: :matrix,
+        fmt: :char,
+        pat: /^(?<v>[a-z]+)[01][01](\k<v>..)*(\s*\.+\s*)?(\k<v>..)+$/i,
+        vars: ->(m) { m[:v] },
+        pat2: ->(v) { /(^#{v}..(#{v}..)*(\s*\.+\s*)?(#{v}..)+|\.+)$/ },
+        size: ->(f) { f[-2..-1].chars.to_a }
+      },
+      {
+        type: :varray,
+        fmt: :number,
+        pat: /^[a-z]+(?<i>[01])(\s+[a-z]+\k<i>)*$/i,
+        vars: ->(m) { m[0].split.map { |w| w[0..-2] } },
+        pat2: lambda { |vs|
+          pat = vs.map { |v| v + '.+' }.join('\s+')
+          /^(#{pat}|\.+)$/
+        },
+        size: ->(f) { /(?<sz>\d+)$/ =~ f ? sz : f[-1] }
+      },
+      {
+        type: :single,
+        fmt: :number,
+        pat: /^[a-z]+(\s+[a-z]+)*$/i,
+        vars: ->(m) { m[0].split },
+        pat2: ->(_) { nil },
+        size: ->(_) { '' }
+      }
+    ].freeze
 
     def process(pbm)
       defs = parse(pbm.fmt, pbm.smps)
@@ -22,52 +92,35 @@ module AtCoderFriends
 
     def parse_fmt(fmt)
       lines = split_trim(fmt)
-      lines << ''
-      inpdefs = []
-      re = prev = nil
-      lines.each do |f|
-        if re
-          if re =~ f
-            prev = f
+      lines << '' # sentinel
+      it = Iterator.new(lines)
+      generator = Enumerator.new do |y|
+        cur = it.next
+        loop do
+          parser = PARSERS.find { |ps| ps[:pat] =~ cur }
+          unless parser
+            break unless it.next?
+            cur = it.next
             next
           end
-          inpdef = inpdefs.last
-          case inpdef.type
-          when :matrix
-            inpdef.size = prev[-2..-1].chars.to_a
-          when :varray
-            inpdef.size = /(?<sz>\d+)$/ =~ prev ? sz : prev[-1]
+          type, fmt = parser.values_at(:type, :fmt)
+          m = parser[:pat].match(cur)
+          vars = parser[:vars].call(m)
+          pat2 = parser[:pat2].call(vars)
+          if pat2
+            while pat2 =~ cur
+              prv = cur
+              cur = it.next
+            end
+          else
+            prv = cur
+            cur = it.next
           end
-          re = prev = nil
-        end
-        case f
-        when /^(?<v>[a-z]+)[01](\s+\k<v>.)*(\s+\.+)?(\s+\k<v>.)+$/i
-          v = $LAST_MATCH_INFO[:v]
-          inpdefs << InputDef.new(:harray, f[-1], :number, v)
-        when /^(?<v>[a-z]+)[01](\k<v>.)*(\s*\.+\s*)?(\k<v>.)+$/i
-          v = $LAST_MATCH_INFO[:v]
-          inpdefs << InputDef.new(:harray, f[-1], :char, v)
-        when /^(?<v>[a-z]+)[01][01](\s+\k<v>..)*(\s+\.+)?(\s+\k<v>..)+$/i
-          v = $LAST_MATCH_INFO[:v]
-          inpdefs << InputDef.new(:matrix, nil, :number, v)
-          re = /(^#{v}..(\s+#{v}..)*(\s+\.+)?(\s+#{v}..)+|\.+)$/
-          prev = f
-        when /^(?<v>[a-z]+)[01][01](\k<v>..)*(\s*\.+\s*)?(\k<v>..)+$/i
-          v = $LAST_MATCH_INFO[:v]
-          inpdefs << InputDef.new(:matrix, nil, :char, v)
-          re = /(^#{v}..(#{v}..)*(\s*\.+\s*)?(#{v}..)+|\.+)$/
-          prev = f
-        when /^[a-z]+(?<i>[01])(\s+[a-z]+\k<i>)*$/i
-          vars = f.split.map { |w| w[0..-2] }
-          inpdefs << InputDef.new(:varray, nil, :number, vars)
-          pat = vars.map { |w| w + '.+' }.join('\s+')
-          re = /^(#{pat}|\.+)$/
-          prev = f
-        when /^[a-z]+(\s+[a-z]+)*$/i
-          inpdefs << InputDef.new(:single, nil, :number, f.split)
+          size = parser[:size].call(prv)
+          y << InputDef.new(type, size, fmt, vars)
         end
       end
-      inpdefs
+      generator.each.to_a
     end
 
     def split_trim(fmt)
